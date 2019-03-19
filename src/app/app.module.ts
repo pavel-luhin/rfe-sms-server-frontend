ed3c6947ac15d6055d150f482d2a93288cb5533f
Pavel Luhin <pavel.luhin@moneyman.ru>
2019-03-19 22:04:04 +0300

    add components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StatisticsComponent } from './statistics/statistics.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StatisticsComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/statistics/statistics.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { Http } from '@angular/http';
+
+@Component({
+  selector: 'app-statistics',
+  templateUrl: './statistics.component.html',
+  styleUrls: ['./statistics.component.css']
+})
+export class StatisticsComponent implements OnInit {
+
+  constructor(private http: Http) { }
+
+  ngOnInit() {
+  	this.http.get()
+  }
+
+}
